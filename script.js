8c03abd6d5dd8b4ceaab3de8d666b1baf4d7a1f0
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-01-28 13:41:33 +0600

    improved by myself

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
-// login button event handler
-const loginBtn = document.getElementById('login');
-loginBtn.addEventListener('click', () => {
-    const loginArea = document.getElementById('login-area');
-    loginArea.style.display = 'none';
-    const transactionArea = document.getElementById('transaction-area');
-    transactionArea.style.display = 'block';
-});
+// hide login area and showing bank area by clicking enter
 
+const enter = document.getElementById('enter');
+enter.addEventListener('click', () => {
+    // hide
+    const hideArea = document.getElementById('login-area');
+    hideArea.style.display = 'none';
 
-// deposit button event handler
-
-const depositBtn = document.getElementById('addDeposit');
-depositBtn.addEventListener('click', () => {
-    const depositNumber = getInputNumber('depositAmount');
-
-    updateSpanText('currentDeposit', depositNumber);
-
-    updateSpanText('currentBalance', depositNumber);
-
-    document.getElementById('depositAmount').value = '';
-});
-
-
-// withdraw button event handler
-
-const withdrawBtn = document.getElementById('addWithdraw');
-withdrawBtn.addEventListener('click', () => {
-    const withdrawNumber = getInputNumber('withdrawAmount');
-
-    updateSpanText('currentWithdraw', withdrawNumber);
-    updateSpanText('currentBalance', -1 * withdrawNumber)
-
-    document.getElementById('withdrawAmount').value = '';
+    // show
+    const showArea = document.getElementById('bank-area');
+    showArea.style.display = 'block';
 })
 
-function getInputNumber(id) {
-    const amount = document.getElementById(id).value;
-    const amountNumber = parseFloat(amount);
-    return amountNumber;
+
+// deposit
+
+const depositBtn = document.getElementById('deposit-in-btn');
+depositBtn.addEventListener('click', () => {
+    add('deposit-in', 'deposit-amount', 'warning-text');
+    add('deposit-in', 'balance-amount');
+
+    warning('deposit-in', 'warning-text');
+    warningNumber('deposit-in', 'warning-text');
+
+    emptyValue('deposit-in');
+});
+
+// withdraw
+
+const withdrawBtn = document.getElementById('withdraw-in-btn');
+withdrawBtn.addEventListener('click', () => {
+    add('withdraw-in', 'withdraw-amount');
+    deducate('withdraw-in', 'balance-amount');
+
+    warning('withdraw-in', 'warning-text-2');
+    warningNumber('withdraw-in', 'warning-text-2');
+
+    emptyValue('withdraw-in');
+});
+
+function emptyValue(id) {
+    target = document.getElementById(id);
+    target.value = '';
 }
 
-function updateSpanText(id, depositNumber) {
-    const current = document.getElementById(id).innerText;
-    const currentNumber = parseFloat(current);
-    const total = depositNumber + currentNumber;
-    document.getElementById(id).innerText = total;
+function add(id, addthen) {
+    const input = document.getElementById(id).value;
+    const inputNumber = parseFloat(input);
+
+    const addTo = document.getElementById(addthen).innerText;
+    const addToNumber = parseFloat(addTo);
+
+    const totalValue = inputNumber + addToNumber;
+    if (input !== '' && Number.isNaN(inputNumber) == false) {
+        document.getElementById(addthen).innerText = totalValue;
+    }
+}
+
+function deducate(id, deduct) {
+    const input = document.getElementById(id).value;
+    const inputNumber = parseFloat(input);
+
+    const deductTo = document.getElementById(deduct).innerText;
+    const deductToNumber = parseFloat(deductTo);
+
+    const totalValue = deductToNumber - inputNumber;
+    if (input !== '' && Number.isNaN(inputNumber) == false) {
+        document.getElementById(deduct).innerText = totalValue;
+    }
+}
+
+function warning(id, warnTo) {
+    const element = document.getElementById(id).value;
+    const warnToElement = document.getElementsByClassName(warnTo);
+
+
+    for (var i = 0; i < warnToElement.length; i++) {
+        if (element == '') {
+            warnToElement[i].innerText = "Input field can't be empty";
+        } else {
+            warnToElement[i].innerText = '';
+        }
+    }
+}
+
+
+function warningNumber(id, warnTo) {
+    input = document.getElementById(id).value;
+    const warnToElement = document.getElementsByClassName(warnTo);
+
+    for (var i = 0; i < warnToElement.length; i++) {
+        if (isNaN(input) == true) {
+            warnToElement[i].innerText = 'Please input number';
+        } else {
+            warnToElement[i].innerText = '';
+        }
+    }
 }
